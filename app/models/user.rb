7cf76761129240c2608d6bb310f76670a050bcc0
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2020-10-31 18:50:32 +0200

    Task 12. OAuth with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,37 @@ class User < ApplicationRecord
   has_many :answers,   dependent: :destroy
   has_many :awards
   has_many :votes
+  has_many :authorizations
 
   def author_of?(resource)
     id.present? && id == resource.try(:user_id)
   end
 
-  def self.find_by_oauth(oauth); end
+  def self.find_by_oauth(oauth)
+    authorization = Authorization.where(
+      provider: oauth.provider,
+      uid: oauth.uid.to_s
+    ).first
+
+    return authorization.user if authorization
+
+    email = oauth.info[:email]
+    user  = User.where(email: email).first
+
+    unless user
+      password = Devise.friendly_token(128)
+      user = User.create!(
+        email: email,
+        password: password,
+        password_confirmation: password
+      )
+    end
+
+    user.authorizations.create(
+      provider: oauth.provider,
+      uid: oauth.uid
+    )
+
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,83 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_author_of(resource.new(nil))
     end
   end
+
+  describe 'find_by_oauth method' do
+    context 'user has authorization' do
+      let(:user) { create :user }
+      let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: 123_456) }
+
+      it 'returns user' do
+        user.authorizations.create(provider: 'facebook', uid: 123_456)
+        expect(User.find_by_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has no authorization' do
+      let!(:user) { create :user }
+      let(:auth) do
+        OmniAuth::AuthHash.new(
+          provider: 'facebook',
+          uid: 123_456,
+          info: { email: user.email }
+        )
+      end
+
+      context 'user already exists' do
+        it "doesn't create a new user" do
+          expect { User.find_by_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'creates new authorization for a user' do
+          expect { User.find_by_oauth(auth) }.to change(Authorization, :count).by(1)
+        end
+
+        it 'created authorization contains proper data' do
+          user = User.find_by_oauth(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid.to_s
+        end
+
+        it 'returns user' do
+          expect(User.find_by_oauth(auth)).to eq user
+        end
+      end
+
+      context "user doesn't exist" do
+        let(:auth) do
+          OmniAuth::AuthHash.new(
+            provider: 'facebook',
+            uid: 123_456,
+            info: { email: 'new.user@email.com' }
+          )
+        end
+
+        it 'create a new user' do
+          expect { User.find_by_oauth(auth) }.to change(User, :count).by(1)
+        end
+
+        it "fills user's email" do
+          expect(User.find_by_oauth(auth).email).to eq auth[:info][:email]
+        end
+
+        it 'creates new authorization for a user' do
+          expect { User.find_by_oauth(auth) }.to change(Authorization, :count).by(1)
+        end
+
+        it 'created authorization contains proper data' do
+          user = User.find_by_oauth(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid.to_s
+        end
+
+        it 'returns user' do
+          expect(User.find_by_oauth(auth).class).to eq User
+        end
+      end
+    end
+  end
 end
