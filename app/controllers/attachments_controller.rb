3c1acc1eda0af76f67835d92d2490956538cbf07
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2020-11-09 22:42:16 +0200

    Task 13. Remove unnecessary permissions checks

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,7 +5,6 @@ class AttachmentsController < ApplicationController
   before_action :load_attachment
   before_action :load_model
   authorize_resource
-  before_action :check_permissions
 
   def destroy
     @attachement_id = @attachment.id
@@ -20,15 +19,5 @@ class AttachmentsController < ApplicationController
 
   def load_model
     @model = @attachment.record
-  end
-
-  def check_permissions
-    render_403 unless current_user.author_of?(@model)
-  end
-
-  def render_403
-    render file: File.join(Rails.root, 'public/403.html'),
-           status: :forbidden,
-           layout: false
   end
 end
