3173bc198c232f2df2eb9109f78896546e62a104
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2020-11-09 17:37:40 +0200

    Task 13. Tests

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -7,16 +7,18 @@ describe Ability do
 
   describe 'visitor' do
     let(:user) { nil }
-
-    it { should be_able_to :read, Question }
-    it { should be_able_to :read, Answer }
-
+    
+    it { should be_able_to :read, :all }
     it { should_not be_able_to :manage, :all }
   end
 
   describe 'user' do
-  end
+    let(:user) { create :user }
 
-  describe 'author' do
+    it { should be_able_to :manage, Question }
+    it { should be_able_to :manage, Comment }
+    it { should be_able_to :manage, Vote }
+    it { should be_able_to :manage, ActiveStorage::Attachment }
+    it { should be_able_to [:create, :update, :destroy, :make_best], Answer }
   end
 end
